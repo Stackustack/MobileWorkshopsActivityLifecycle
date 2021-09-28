405776c8f72bc01bc2b7066b19e794ccbcca8eaa
Michał Jung <stackustack@gmail.com>
2021-09-28 15:29:23 +0200

    stuff is done

---
+++ app/src/main/java/com/example/mobileworkshopslifecycle/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.mobileworkshopslifecycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "Mobile Workshops - MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.i(TAG, "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.i(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.i(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.i(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.i(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.i(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        Log.i(TAG, "onRestart");
+    }
+
+    public void openOtherActivity(View v) {
+        Intent intent = new Intent(this, OtherActivity.class);
+        startActivity(intent);
+    }
+
+
+}
